19100d56c5c9e5bd17eb1c6a0bb743fc28b25ae9
anbu <anbu@thelaunchclub.in>
2023-10-19 15:20:18 +0530

    initial commit

---
+++ arun-ios/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  arun-ios
+//
+//  Created by Anbu p on 19/10/23.
+//
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
